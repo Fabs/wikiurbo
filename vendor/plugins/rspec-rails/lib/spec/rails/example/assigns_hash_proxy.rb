cc9fdd36989a077bae9dd85a27138b3234b17887
Rafael Schouery <schouery@gmail.com>
2008-10-03 18:00:40 -0300

    The missing vendor folder

---
+++ vendor/plugins/rspec-rails/lib/spec/rails/example/assigns_hash_proxy.rb
@@ -0,0 +1,37 @@
+module Spec
+  module Rails
+    module Example
+      class AssignsHashProxy #:nodoc:
+        def initialize(example_group, &block)
+          @target = block.call
+          @example_group = example_group
+        end
+
+        def [](key)
+          assigns[key] || assigns[key.to_s] || @target.instance_variable_get("@#{key}")
+        end
+
+        def []=(key, val)
+          @target.instance_variable_set("@#{key}", val)
+        end
+
+        def delete(key)
+          assigns.delete(key.to_s)
+        end
+
+        def each(&block)
+          assigns.each &block
+        end
+
+        def has_key?(key)
+          assigns.key?(key.to_s)
+        end
+
+        protected
+        def assigns
+          @example_group.orig_assigns
+        end
+      end
+    end
+  end
+end
