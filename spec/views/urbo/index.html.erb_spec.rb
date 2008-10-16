47e97c418d0c8b7dbc4cb7fc8cb05273edbf973c
Rafael Schouery <schouery@gmail.com>
2008-10-16 10:17:16 -0200

    Trying to show the map

--- spec/views/urbo/index.html.erb_spec.rb
+++ spec/views/urbo/index.html.erb_spec.rb
@@ -4,6 +4,6 @@ describe "/urbo/index.html.erb" do
   it "should render the map" do
     render "/urbo/index.html.erb"
     response.should have_tag('div#map_div')
+    response.should have_tag('script[src=?][type=?]', '/javascripts/ym4r-gm.js', 'text/javascript')
   end
 end
-
