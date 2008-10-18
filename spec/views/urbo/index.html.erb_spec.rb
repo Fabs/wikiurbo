cb492af533a720531f664d4d46c5a1a6ff327054
Rafael Schouery <schouery@gmail.com>
2008-10-17 22:12:51 -0200

    Setting the width and height for the map

--- spec/views/urbo/index.html.erb_spec.rb
+++ spec/views/urbo/index.html.erb_spec.rb
@@ -6,6 +6,7 @@ describe "/urbo/index.html.erb" do
     assigns[:map] = @map = mock(GMap)
     @map.should_receive(:to_html)
     @map.should_receive(:div).and_return()
+    @map.should_receive(:header_width_height)
     GMap.should_receive(:header)    
     render "/urbo/index.html.erb"
   end
